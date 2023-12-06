c4dd2e102487741311f3a7acbc02e50a0e012fab
TypeSafeSchwalbe <78918080+TypeSafeSchwalbe@users.noreply.github.com>
2023-12-06 16:40:48 +0200

    Put the standard library into a separate repository (#4)
    * Delete stdlib-c directory
    
    * Delete stdlib-core directory
    
    * Delete stdlib-js directory

--- stdlib-c/io.c
+++
@@ -1,8 +0,0 @@
-
-#include <stdio.h>
-#include "gera.h"
-
-void gera_std_io_println(GeraString line) {
-    GERA_STRING_NULL_TERM(line, line_nt);
-    puts(line_nt);
-}
--- stdlib-js/io.js
+++
@@ -1,4 +0,0 @@
-
-function gera_std_io_println(line) {
-    console.log(line);
-}
